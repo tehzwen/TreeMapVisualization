6428f75280664306363cc0551ad32d92905dd410
Zachary Shaw <shawz2@mymacewan.ca>
2019-03-19 17:35:10 -0600

    Added dynamic filtering

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 
 var sectorName = "Energy";
-var valueSelect = "Growth"
+var valueName = "Growth"
 
-var overAllHeight = 800;
-var overAllWidth = 1200;
+var overAllHeight = 1200;
+var overAllWidth = 1800;
+
+var textColor = 'black';
+var themeColor = '#68edbc';
+var boxFontSize = 0;
 
 function main() {
-    initialSetup();
-}
 
+    var myDiv = d3.select('#mydiv')
+    myDiv.insert('h1')
+        .style('text-align','center')
+        .style('color','white')
+        .style('font-family', 'Courier New, Courier, monospace')
+        .text("Growth Rate by Sector")
 
-function initialSetup() {
     var sectorOptions = [
         "Energy",
         "Utilities",
@@ -26,36 +33,87 @@ function initialSetup() {
         "Communication Services"
     ];
 
+    var valueOptions = [
+        "Growth",
+        "PEG",
+        "P/E",
+        "Price",
+        "Dividend"
+    ]
 
 
-    var body = d3.select('body')
-    var myDiv = d3.select('#mydiv')
-
-    myDiv.insert('h1')
-        .attr('class', 'header-text')
-        .text("Growth Rate by Sector")
-
-    sectorSelect = d3.select('#mydiv')
-        .append("div")
-        .append("select")
+    sectorSelect = myDiv
+        .insert("div")
+        .style('margin-left','5vh')
+        .insert("select")
+        .attr('class', 'category-thing')
+        .style('font-family', 'Courier New, Courier, monospace')
+        .style('margin-right', '5vh')
+        .style('background-color', themeColor)
+        .style('color', textColor)
+        .style('width', '10vw')
+        .style('height', '1.5vw')
 
     sectorSelect.selectAll("option")
         .data(sectorOptions)
         .enter()
         .append("option")
         .attr("value", function (d) {
-            console.log(d);
             return d;
         })
         .text(function (d) {
             return d;
         });
 
-    d3.csv("/data/allCCC.csv", function (data) {
-        //console.log(data);
-        //console.log(data["Sector"]);
+    valueSelect = myDiv
+        .select("div")
+        .insert("select")
+        .style('font-family', 'Courier New, Courier, monospace')
+        .style('background-color', themeColor)
+        .style('color', textColor)
+        .style('width', '10vw')
+        .style('height', '1.5vw')
 
-        if (data["Sector"] === sectorName) {
+
+    valueSelect.selectAll("option")
+        .data(valueOptions)
+        .enter()
+        .append("option")
+        .attr("value", function (d) {
+            return d;
+        })
+        .text(function (d) {
+            return d;
+        });
+
+
+    getData(sectorName, valueName);
+
+    valueSelect.on("change", function (d) {
+        valueName = d3.select(this).property("value");
+        getData(sectorName, valueName);
+    });
+
+    sectorSelect.on("change", function (d) {
+        sectorName = d3.select(this).property("value");
+        getData(sectorName, valueName);
+    });
+}
+
+
+function getData(sector, value) {
+
+    d3.select("svg").remove();
+    d3.select("body")
+        .append("svg")
+        .style("width", overAllWidth)
+        .style("height", overAllHeight)
+        .style('margin-left','5vh')
+        .append("g");
+
+    d3.csv("/data/allCCC.csv", function (data) {
+
+        if (data["Sector"] === sector) {
             return data;
         }
 
@@ -64,120 +122,130 @@ function initialSetup() {
 
         for (let i = 0; i < sectorData.length; i++) {
 
-            if (sectorData[i]["Growth"] > 10) {
+
+            if (sectorData[i][value] > 0 && sectorData[i][value] !== "n/a") {
+                console.log(sectorData[i][value]);
                 actualData.children.push({
                     "name": sectorData[i]["Name"],
-                    "value": sectorData[i][valueSelect]
+                    "value": sectorData[i][value]
                 });
             }
         }
 
-        var treemapLayout = d3.treemap()
-            .size([overAllWidth, overAllHeight])
-            .paddingOuter(10);
+        displayTreeMap(actualData);
 
-        var root = d3.hierarchy(actualData)
-
-        root.sum(function (d) {
-            return d.value;
-        });
-
-        treemapLayout(root);
-
-        var blocks = d3.select('svg g')
-            .selectAll('rect')
-            .data(root.descendants())
-            .append('rect')
-            .attr('x', function (d) { return d.x0; })
-            .attr('y', function (d) { return d.y0; })
-            .attr('width', function (d) { return d.x1 - d.x0; })
-            .attr('height', function (d) { return d.y1 - d.y0; })
-
-        blocks.enter()
-
-
-        var nodes = d3.select('svg g')
-            .selectAll('g')
-            .data(root.descendants())
-            .enter()
-            .append('g')
-            .attr('transform', function (d) {
-                return 'translate(' + [d.x0, d.y0] + ')'
-            })
-
-        nodes
-            .append('rect')
-            .attr('width', function (d) { return d.x1 - d.x0; })
-            .attr('height', function (d) { return d.y1 - d.y0; })
-
-        nodes
-            .append('text')
-            .attr('class', 'block-text')
-            .text(function (d) {
-                return d.data.name;
-            })
-            .attr('dx', 4)
-            .attr('dy', 14)
-
-
-        nodes
-            .append('text')
-            .text(function (d) {
-                return d.data.value;
-            })
-            .attr('dx', 4)
-            .attr('dy', 35)
-
-        d3.selectAll('rect')
-            .attr('fill', 'cadetblue')
-            .attr('stroke', 'white')
-            .attr('fill-opacity', function (d) {
-
-                let opacityVal = 0.01 * d.data.value;
-
-                if (opacityVal) {
-                    return opacityVal;
-                }
-                else {
-                    return 0.01;
-                }
-            })
 
     });
+}
+
+function getAverage(actualData) {
+    var total = 0.0;
+
+    for (let i = 0; i < actualData.children.length; i++) {
+        if (actualData.children[i].value !== "n/a") {
+            total += parseFloat(actualData.children[i].value);
+        }
+
+    }
+
+    var avg = total / actualData.children.length;
+    console.log(avg);
+    return avg;
+}
+
+function displayTreeMap(actualData) {
+    //console.log(actualData);
+    var avg = getAverage(actualData);
+
+    var treemapLayout = d3.treemap()
+        .size([overAllWidth, overAllHeight])
+        .tile(d3.treemapSquarify)
+        .paddingOuter(10);
+
+    var root = d3.hierarchy(actualData)
+
+    root.sum(function (d) {
+        return d.value;
+    });
+
+    treemapLayout(root);
+
+    var blocks = d3.select('svg g')
+        .selectAll('rect')
+        .data(root.descendants())
+        .append('rect')
+        .attr('x', function (d) { return d.x0; })
+        .attr('y', function (d) { return d.y0; })
+        .attr('width', function (d) { return d.x1 - d.x0; })
+        .attr('height', function (d) { return d.y1 - d.y0; })
 
 
-    sectorSelect
-        .on("change", function (d) {
-            sectorName = d3.select(this).property("value");
-            alert(sectorName);
-
-            d3.csv("/data/allCCC.csv", function (data) {
-                if (data["Sector"] === sectorName) {
-                    return data;
-                }
-
-            }).then((sectorData) => {
-                var actualData = { "name": "", "children": [] };
-
-                for (let i = 0; i < sectorData.length; i++) {
-
-                    if (sectorData[i]["Growth"] > 10) {
-                        actualData.children.push({
-                            "name": sectorData[i]["Name"],
-                            "value": sectorData[i][valueSelect]
-                        });
-                    }
-                }
-
-                blocks.exit().remove()
-
-            })
+    blocks.enter()
 
 
-        });
+    var nodes = d3.select('svg g')
+        .selectAll('g')
+        .data(root.descendants())
+        .enter()
+        .append('g')
+        
+        .attr('transform', function (d) {
+            return 'translate(' + [d.x0, d.y0] + ')'
+        })
+
+
+    nodes
+        .append('rect')
+        
+        .attr('width', function (d) { return d.x1 - d.x0; })
+        .attr('height', function (d) { return d.y1 - d.y0; })
+
+    nodes
+        .append('text')
+        .style('font-family','Courier New, Courier, monospace')
+        .style('color', textColor)
+        .text(function (d) {
+            return d.data.name;
+        })
+        .attr('dx', 4)
+        .attr('dy', 14)
+
+
+    nodes
+        .append('text')
+        .style('font-weight','bold')
+        .style('font-family','Courier New, Courier, monospace')
+        .style('font-color', textColor)
+        .text(function (d) {
+            return d.data.value;
+        })
+        
+        .attr('dx', 4)
+        .attr('dy', 35)
+
+
+    d3.selectAll('rect')
+        .attr("id", "data-square")
+        .attr('fill', themeColor)
+        .attr('stroke', 'white')
+        .attr('fill-opacity', function (d) {
+
+            let opacityVal;
+
+            opacityVal = d.data.value / (avg * 2);
+
+            if (opacityVal) {
+                return opacityVal;
+            }
+            else {
+                return 0.01;
+            }
+        })
+
 
 
 }
+
 
 
 
